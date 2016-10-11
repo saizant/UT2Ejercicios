425e5d42860184d1d93907634f6964d659768555
Toni <Toni@PC>
2016-10-11 12:57:48 +0200

    Ejercicio 1 realizado.

--- Ejercicio1/src/CalcPrimeNumbersThread.java
+++ Ejercicio1/src/CalcPrimeNumbersThread.java
@@ -1,4 +1,26 @@
+import java.awt.List;
+import java.util.Scanner;
 
 public class CalcPrimeNumbersThread extends Thread {
-	// COMPLETAR AQUÍ EL EJERCICIO
+	// COMPLETAR AQUÃ� EL EJERCICIO
+	private int num1;
+	private int num2;
+	
+	public CalcPrimeNumbersThread(String[] numeros) {
+		super("CalcPrimeNumbersThread");
+		num1 = Integer.parseInt(numeros[0]);
+		num2 = Integer.parseInt(numeros[1]);
+	}
+	
+	
+	public void run(){		
+				
+		List<Integer> primos = PrimeNumbers.primesInRange(num1, num2);
+		
+		for (Integer primo : primos) {
+			System.out.println(primo);
+		}
+		
+	}
+	
 }
--- Ejercicio1/src/PrimeNumbersInRange.java
+++ Ejercicio1/src/PrimeNumbersInRange.java
@@ -1,17 +1,33 @@
-// Queremos calcular los números primos entre 2 números y imprimirlos por pantalla.
+import java.util.Scanner;
+
+// Queremos calcular los nÃºmeros primos entre 2 nÃºmeros y imprimirlos por pantalla.
 //
 //Requisitos:
 //
-//    lee de la entrada estándar 2 números separados por espacio en la primera linea. (Ej: "2 100")
+//    lee de la entrada estÃ¡ndar 2 nÃºmeros separados por espacio en la primera linea. (Ej: "2 100")
 //    Utiliza un thread que realice el trabajo y se encargue de imprimir por pantalla la lista resultante.
-//    Utiliza el método de extender un thread (extends Thread)
-//    El thread principal debe esperar a la finalización del thread trabajador o "worker".
+//    Utiliza el mÃ©todo de extender un thread (extends Thread)
+//    El thread principal debe esperar a la finalizaciÃ³n del thread trabajador o "worker".
 
 
 public class PrimeNumbersInRange {
 
 	public static void main(String[] args) {
-		// COMPLETA AQUÍ EL EJERCICIO
+		// COMPLETA AQUÃ� EL EJERCICIO
+		
+		System.out.println("Introduzca 2 números separados por espacio: ");
+		String[] numeros = new Scanner(System.in).nextLine().split(" ");
+		
+		Thread t = new CalcPrimeNumbersThread(numeros);
+		t.start();
+		
+		try {
+			t.join();
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+		
+		System.out.println("Thread acabado. ");
 
 	}
 
