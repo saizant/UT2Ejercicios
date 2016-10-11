59805df665a368f2df106db1e5555de7ddd024ee
Toni <Toni@PC>
2016-10-11 13:09:21 +0200

    Ejercicio 2 realizado.

--- Ejercicio2/src/CalcPrimeNumbersRunnable.java
+++ Ejercicio2/src/CalcPrimeNumbersRunnable.java
@@ -1,15 +1,26 @@
+import java.awt.List;
 
 public class CalcPrimeNumbersRunnable implements Runnable {
 	private int from;
 	private int to;
 
-	public void CalcPrimeNumbersRunnable(int from, int to) {
+	public CalcPrimeNumbersRunnable(int from2, int to2) {
+		// TODO Auto-generated constructor stub
 		this.from = from;
 		this.to= to;
-    }
+	}
+//	public void CalcPrimeNumbersRunnable(int from, int to) {
+//		this.from = from;
+//		this.to= to;
+//    }
 	@Override
 	public void run() {
-		// COMPLETAR AQUÍ EL EJERCICIO
+		// COMPLETAR AQUÃ� EL EJERCICIO
+		List<Integer> primos = PrimeNumbers.primesInRange(from, to);
+		
+		for (Integer primo : primos) {
+			System.out.println(primo);
+		}
 	}
 
 }
--- Ejercicio2/src/PrimeNumbersInRange.java
+++ Ejercicio2/src/PrimeNumbersInRange.java
@@ -1,17 +1,34 @@
-// Queremos calcular los números primos entre 2 números y imprimirlos por pantalla.
+import java.util.Scanner;
+
+// Queremos calcular los nÃºmeros primos entre 2 nÃºmeros y imprimirlos por pantalla.
 //
 //Requisitos:
 //
-//    lee de la entrada estándar 2 números separados por espacio en la primera linea. (Ej: "2 100")
+//    lee de la entrada estÃ¡ndar 2 nÃºmeros separados por espacio en la primera linea. (Ej: "2 100")
 //    Utiliza un thread que realice el trabajo y se encargue de imprimir por pantalla la lista resultante.
-//    Utiliza el método de implementar Runnable.
-//    El thread principal debe esperar a la finalización del thread trabajador o "worker".
+//    Utiliza el mÃ©todo de implementar Runnable.
+//    El thread principal debe esperar a la finalizaciÃ³n del thread trabajador o "worker".
 
 
 public class PrimeNumbersInRange {
 
 	public static void main(String[] args) {
-		// COMPLETA AQUÍ EL EJERCICIO
+		// COMPLETA AQUÃ� EL EJERCICIO
+		System.out.println("Introduzca 2 números separados por espacio: ");
+		String[] numeros = new Scanner(System.in).nextLine().split(" ");
+		
+		int from = Integer.parseInt(numeros[0]);
+		int to = Integer.parseInt(numeros[1]);
+		
+		Runnable r = new CalcPrimeNumbersRunnable(from, to);
+		
+		new Thread(r).start();
+		
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException ex ) {
+			ex.printStackTrace();
+		}
 
 	}
 
