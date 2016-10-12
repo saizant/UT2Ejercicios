c0caadd277a6a60470ee89f2ee119f647e7ed1d6
Toni <saizant@gmail.com>
2016-10-12 04:42:04 +0200

    Ejercicio 3 realizado

--- Ejercicio3/src/CalcPrimeNumbersRunnable.java
+++ Ejercicio3/src/CalcPrimeNumbersRunnable.java
@@ -1,12 +1,13 @@
+import java.awt.List;
 
 public class CalcPrimeNumbersRunnable implements Runnable {
 	private int id;
 	private int from;
 	private int to;
 	private Thread previous;
-	
+
 	// Nos guardamos el id, inicio, fin, y referencia al Runnablethread anterior
-	public void CalcPrimeNumbersRunnable(int id, int from, int to, Thread previous) {
+	public CalcPrimeNumbersRunnable(int id, int from, int to, Thread previous) {
 		this.id = id;
 		this.from = from;
 		this.to= to;
@@ -15,7 +16,23 @@ public class CalcPrimeNumbersRunnable implements Runnable {
 	
 	@Override
 	public void run() {
-		// COMPLETAR AQUÍ EL EJERCICIO
+		// COMPLETAR AQUÃ� EL EJERCICIO
+		
+		List<Integer> primos = PrimeNumbers.primesInRange(from, to);
+		
+		if (previous != null) {
+			while(previous.isAlive()){
+				//
+			}
+		}
+		
+		System.out.println("ID del thread: " + id);
+		for (Integer primo : primos) {
+			System.out.print(primo);
+			System.out.print(",");
+		}
+		
+		
 	}
 
 }
--- Ejercicio3/src/PrimeNumbersInRange.java
+++ Ejercicio3/src/PrimeNumbersInRange.java
@@ -1,26 +1,54 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
-// Queremos calcular los números primos entre 2 números y imprimirlos por pantalla.
+// Queremos calcular los nÃºmeros primos entre 2 nÃºmeros y imprimirlos por pantalla.
 //
-//  lee de la entrada estándar 2 números separados por espacio en la primera linea. (Ej: "1 100")
-//  lee de la segunda linea de la entrada estándar el número de threads en los que se tiene que dividir el trabajo.
-//  Cada thread debe encargarse de realizar una parte equivativa del cálculo
+//  lee de la entrada estÃ¡ndar 2 nÃºmeros separados por espacio en la primera linea. (Ej: "1 100")
+//  lee de la segunda linea de la entrada estÃ¡ndar el nÃºmero de threads en los que se tiene que dividir el trabajo.
+//  Cada thread debe encargarse de realizar una parte equivativa del cÃ¡lculo
 //
 //    thread 1 [1..50]
 //    thread 2 [51..100]
 //
 //Cada thread debe imprimir por pantalla su id, seguido de la lista de primos en su rango.
-//Los ids serán asignados en orden incremental por el thread principal de 1 a N.
+//Los ids serÃ¡n asignados en orden incremental por el thread principal de 1 a N.
 //Cada thread siempre tiene que esperar a que finalice el thread anterior antes de imprimir!
-//Utiliza el método de implementar Runnable.
-//El thread principal debe esperar a la finalización del thread con mayor id (el último).
+//Utiliza el mÃ©todo de implementar Runnable.
+//El thread principal debe esperar a la finalizaciÃ³n del thread con mayor id (el Ãºltimo).
 
 public class PrimeNumbersInRange {
 	// Ayuda: utiliza esta lista para guardarte la referencia a los threads que crees
 	private ArrayList<Thread> threads; 
 
 	public static void main(String[] args) {
-		// COMPLETA AQUÍ EL EJERCICIO
+		// COMPLETA AQUÃ� EL EJERCICIO
+		
+		System.out.println("Introduzca 2 números separados por espacio: ");
+		String[] numeros = new Scanner(System.in).nextLine().split(" ");
+		
+		System.out.println("Introduzca el número de threads: ");
+		String n_hilos = new Scanner(System.in).nextLine();
+		
+		int from = Integer.parseInt(numeros[0]);
+		int to = Integer.parseInt(numeros[1]);
+		
+		int hilos = Integer.parseInt(n_hilos);
+		int prop = (to - from) / hilos;
+		
+		Runnable r;
+		Thread t = null;
+		
+		for (int i = 0; i < hilos; i++) {
+			r = new CalcPrimeNumbersRunnable(i + 1, from+(i*prop), from+((i + 1)*prop), t);
+			t = new Thread(r);
+			t.start();
+		}
+		
+		while (t.isAlive()) {
+			//
+		}
+		
+		System.out.println("Thread acabado. ");
 
 	}
 
